28dcf17ec5e50676af95a2981d4742830bcb2b98
Yash gangan <127832558+yashgangan32@users.noreply.github.com>
2024-11-14 02:13:19 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -46,7 +46,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/yashgangan32",
   resume:
-    "https://drive.google.com/file/d/1zuj4qob31qdN-NFbQHyMpSF6Xwy4X9zs/view?usp=sharing",
+    "https://drive.google.com/file/d/1asEH_qOT0ItsTlecMB6eqU9__YLEf_ri/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/gangan-yash",
 };
 
